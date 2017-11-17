6e68b5874572c291f42e9e141a36f21ddea5be2d
Daniel Spindelbauer <spindel.daniel97@gmail.com>
2017-11-17 20:50:48 +0000

    - Get GPS coordinates - Connect to WiFi to get time

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from network import LoRa
-import socket
+import pycom
+import machine
+import math
+import network
+import os
 import time
+import utime
+from machine import RTC
+from machine import SD
+from machine import Timer
+from L76GNSS import L76GNSS
+from pytracking import Pytrack
+from network import WLAN
+import gc
 
-lora = LoRa(mode=LoRa.LORA, frequency=863000000)
-s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-s.setblocking(False)
+wlan = WLAN(mode=WLAN.STA)
+wlan.connect('<ssid>', auth=(WLAN.WPA2, '<password>'), timeout=5000)
+while not wlan.isconnected():
+    machine.idle() # save power while waiting
+print('WLAN connection succeeded!')
+# setup rtc
+rtc = machine.RTC()
+rtc.ntp_sync("pool.ntp.org")
+utime.sleep_ms(750)
+print('\nRTC Set from NTP to UTC:', rtc.now())
 
+pycom.heartbeat(False)
+gc.enable()
+
+py = Pytrack()
+l76 = L76GNSS(py, timeout=30)
+chrono = Timer.Chrono()
+chrono.start()
 while True:
-    data = s.recv(64)
-    if data == b'Ping':
-        s.send('Pong')
-        print("got ping")
-    else:
-        print(data)
-    time.sleep_ms(500)
+    print("{} - {}".format(l76.coordinates(), rtc.now()))
